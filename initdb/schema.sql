c76b9f75986ee2c94408d058f1bb38e5ea1ecdfa
Polina Bezukladnova <bezukladnovap@gmail.com>
2025-11-07 16:56:50 +0100

    design an appropriate SQL schema

---
+++ initdb/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE locations(
+  location_id INT  PRIMARY KEY,
+  parent_location_id INT REFERENCES locations(location_id),
+  location_name TEXT NOT NULL
+);
+
+CREATE TABLE devices(
+  device_id INT  PRIMARY KEY,
+  device_type TEXT NOT NULL,
+  device_name TEXT NOT NULL,
+  location_id INT REFERENCES locations(location_id)
+);
+
+CREATE TABLE events(
+  event_id INT PRIMARY KEY,
+  device_id INT REFERENCES devices(device_id),
+  "timestamp" TIMESTAMP,
+  details JSONB NOT NULL
+);
