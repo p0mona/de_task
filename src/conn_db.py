45a876790121ead804da55b57e290ecddb4a9ae8
Polina Bezukladnova <bezukladnovap@gmail.com>
2025-11-08 11:44:06 +0100

    replace tabs with 4 spaces to comply with PEP8

--- src/conn_db.py
+++ src/conn_db.py
@@ -3,22 +3,22 @@ import logging
 import os
 
 if not os.path.exists("logs"):
-  os.mkdir("logs")
+    os.mkdir("logs")
 
 logging.basicConfig(
-  level=logging.INFO, 
-  filename='logs/conn_db.log', 
-  format="%(asctime)s -  %(levelname)s - %(message)s"
+    level=logging.INFO, 
+    filename='logs/conn_db.log', 
+    format="%(asctime)s -  %(levelname)s - %(message)s"
 )
 
 try:
-  connection = psycopg2.connect(
-    dbname='postgres', 
-    user='postgres', 
-    password='postgres',
-    host='localhost',
-    port=5430
-  )
-  logging.info("Connected!")
+    connection = psycopg2.connect(
+        dbname='postgres', 
+        user='postgres', 
+        password='postgres',
+        host='localhost',
+        port=5430
+    )
+    logging.info("Connected!")
 except:
-  logging.error("Connection ERROR!")
+    logging.error("Connection ERROR!")
