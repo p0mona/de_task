6c763f3ce73d508903c99d385de1797392cfec1b
Polina Bezukladnova <bezukladnovap@gmail.com>
2025-11-10 18:06:35 +0100

    add .env file for environment variables

--- src/conn_db.py
+++ src/conn_db.py
@@ -1,13 +1,17 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def db_connection():
     try:
         connection = psycopg2.connect(
-            dbname='postgres', 
-            user='postgres', 
-            password='postgres',
-            host='localhost',
-            port=5430
+            dbname = os.getenv('PGDBNAME'),
+            user = os.getenv('PGUSER'),
+            password = os.getenv('PGPASS'),
+            host = os.getenv('PGHOST'),
+            port = os.getenv('PGPORT')
         )
         return connection
     except:
