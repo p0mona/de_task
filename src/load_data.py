711a48001dac3d7a1b59a2c0178765fa606cd752
Polina Bezukladnova <bezukladnovap@gmail.com>
2025-11-11 15:19:59 +0100

    create separate method for reading json

--- src/load_data.py
+++ src/load_data.py
@@ -1,19 +1,14 @@
-import json
 from conn_db import db_connection
 import parser
 import logging_config
 import logging
+import open_json
 
 logger = logging.getLogger(__name__)
 
-with open('data/locations.json', 'r', encoding='utf-8') as locations:
-    locations = json.load(locations)
-
-with open('data/devices.json', 'r', encoding='utf-8') as devices:
-    devices = json.load(devices)
-    
-with open('data/events.json', 'r', encoding='utf-8') as events:
-    events = json.load(events)
+locations = open_json('data/locations.json')
+devices = open_json('data/devices.json')
+events = open_json('data/events.json')
 
 def load_data():
     connection = db_connection()
