eab0184a0aa240d685555a069213bcfb1e558da2
Polina Bezukladnova <bezukladnovap@gmail.com>
2025-11-11 16:27:48 +0100

    refactor parsers to use oop and solid

--- src/parser.py
+++ src/parser.py
@@ -1,39 +1,42 @@
-from typing import List, Dict, Tuple
-import copy
-import logging
 import json
+import copy
+from constants import *
+from typing import List, Dict, Tuple
 import logging_config
+import logging
 
 logger = logging.getLogger(__name__)
 
+class Parser:
+    def parse(self, data: List[Dict]) -> List[Tuple]:
+        pass
+    def log(self, name: str):
+        logger.info(f"{name} were successfully parsed.")
 
-def locations_parser(data: List[Dict]) -> List[Tuple]:
-    records = []
-    for item in data:
-        record = (item["location_id"], item["parent_location_id"], item["location_name"])
-        records.append(record)
-    logger.info('Locations were successfully parsed.')
-    return records
+class LocationsParse(Parser):
+    def parse(self, data: List[Dict]) -> List[Tuple]:
+        records = [(item[LOCATION_ID], item[PARENT_LOCATION_ID], item[LOCATION_NAME]) for item in data]
+        self.log("Locations")
+        return records
+  
+class DevicesParse(Parser):
+    def parse(self, data: List[Dict]) -> List[Tuple]:
+        records = [(item[DEVICE_ID], item[DEVICE_TYPE], item[DEVICE_NAME], item[LOCATION_ID]) for item in data]
+        self.log("Devices")
+        return records
+  
+class EventsParse(Parser):
+    def parse(self, data: List[Dict]) -> List[Tuple]:
+        records = []
+        for item in data:
+            details_copy = copy.deepcopy(item[DETAILS])
 
-def devices_parser(data: List[Dict]) -> List[Tuple]:
-    records = []
-    for item in data:
-        record = (item["device_id"], item["device_type"], item["device_name"], item["location_id"])
-        records.append(record)
-    logger.info('Devices were successfully parsed.')
-    return records
+            if DEVICE_ID in details_copy:
+                del details_copy[DEVICE_ID]
+            if TIMESTAMP in details_copy:
+                del details_copy[TIMESTAMP]
 
-def events_parser(data: List[Dict]) -> List[Tuple]:
-    records = []
-    for item in data:
-        details_copy = copy.deepcopy(item['details'])
-
-        if 'device_id' in details_copy:
-            del details_copy["device_id"]
-        if 'timestamp' in details_copy:
-            del details_copy["timestamp"]
-
-        record = (item["event_id"], item["details"]["device_id"], item["details"]["timestamp"], json.dumps(details_copy))
-        records.append(record)
-    logger.info('Events were successfully parsed.')
-    return records
+            record = (item[EVENT_ID], item[DETAILS][DEVICE_ID], item[DETAILS][TIMESTAMP], json.dumps(details_copy))
+            records.append(record)
+        self.log("Events")
+        return records
