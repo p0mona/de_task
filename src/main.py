a4f356c5c5fefa98b1296a1486079a2d529b32e0
Polina Bezukladnova <bezukladnovap@gmail.com>
2025-11-13 11:46:45 +0100

    add if __name__ == __main__ to main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 from helpers import DBManager, LocationsParse, DevicesParse, EventsParse
 
-db = DBManager()
-db.conn_db()
+def main():
+    db = DBManager()
+    db.conn_db()
 
-db.load_data('data/locations.json', LocationsParse, 'locations')
-db.load_data('data/devices.json', DevicesParse, 'devices')
-db.load_data('data/events.json', EventsParse, 'events')
+    db.load_data('data/locations.json', LocationsParse, 'locations')
+    db.load_data('data/devices.json', DevicesParse, 'devices')
+    db.load_data('data/events.json', EventsParse, 'events')
 
-db.conn.commit()
-db.conn.close()
+    db.conn.commit()
+    db.conn.close()
+
+if __name__ == "__main__":
+    main()
