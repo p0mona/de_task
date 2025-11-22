4b28833a4cb6c791e7ee0b55523223c63c96b0e7
Polina Bezukladnova <bezukladnovap@gmail.com>
2025-11-22 09:59:23 +0100

    add test for cli

---
+++ tests/test_cli.py
@@ -0,0 +1,29 @@
+from src.helpers import arg_parse
+import pytest
+
+@pytest.mark.parametrize(
+    'argv, locations, devices, events, format',
+    [(
+        ['main.py', '--locations', 'data/locations.py', '--devices', 'data/devices.py', '--events', 'data/events.py', '--format', 'json'],
+        'data/locations.py',
+        'data/devices.py',
+        'data/events.py',
+        'json'
+    ),
+    (
+        ['main.py', '--locations', 'data/locations.py', '--devices', 'data/devices.py', '--events', 'data/events.py', '--format', 'xml'],
+        'data/locations.py',
+        'data/devices.py',
+        'data/events.py',
+        'xml'
+    )]
+)
+
+def test_arg_parse(mocker, argv, locations, devices, events, format):
+    mocker.patch('sys.argv', argv)
+
+    args = arg_parse()
+    assert args.locations == locations
+    assert args.devices == devices
+    assert args.events == events
+    assert args.format == format
